a461efcfbf3539d0e516c90edd4cb1d09ed66729
nitishmeswal <nitish.meswal@gmail.com>
2024-11-18 20:19:45 +0530

    components.json components/header.tsx components/sidebar.tsx components/ui/button.tsx components/ui/header.tsx components/ui/input.tsx components/ui/separator.tsx components/ui/sheet.tsx components/ui/sidebar.tsx components/ui/skeleton.tsx components/ui/tooltip.tsx hooks/use-mobile.tsx lib/utils.ts

---
+++ components/sidebar.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { SidebarProvider } from './ui/sidebar'
+
+const Sidebar = () => {
+  return (
+    <div>
+      <SidebarProvider/>
+    </div>
+  )
+}
+
+export default Sidebar
