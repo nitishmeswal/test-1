3bf42bc230cc73289fbaa92375df2c7808b1da6e
nitishmeswal <nitish.meswal@gmail.com>
2024-11-21 16:01:57 +0530

    M: components/header.tsx,

--- components/header.tsx
+++ components/header.tsx
@@ -11,9 +11,9 @@ const Header = () => {
       <div className="flex flex-row justify-evenly w-full items-center">
 
         <div id='logo' className=' flex justify-center mr-16 items-center'>
-            <a href='/'>
+            <Link href='/'>
             <Image   src={logo} alt='logo' height={42} />
-            </a>
+            </Link>
         </div>
         <div className="w-full flex  flex-row items-center bg-black rounded-full mx-4 mr-10 px-4 py-2">
             <Image src={search} alt='search' height={6} width={6} className="w-6"/>
