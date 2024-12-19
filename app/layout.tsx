ed69d3bc671e58074c58bd9daa9794adf7657b06
nitishmeswal <nitish.meswal@gmail.com>
2024-12-19 13:31:37 +0530

    M: app/layout.tsx,

--- app/layout.tsx
+++ app/layout.tsx
@@ -64,7 +64,6 @@ export default function RootLayout({
                   <div>
                   </div>
                 </div>
-                  <Footer/>
               </div>
             </SidebarProvider>
           </ThemeProvider>
