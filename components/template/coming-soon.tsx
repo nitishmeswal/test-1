bc302f518fe63065c98bd64631df289d9afd3b2d
nitishmeswal <nitish.meswal@gmail.com>
2024-12-26 16:00:05 +0530

    M: components/template/coming-soon.tsx,

--- components/template/coming-soon.tsx
+++ components/template/coming-soon.tsx
@@ -8,7 +8,7 @@ import { Button } from "../ui/button";
 const ComingSoon = () => {
   const route = useRouter()
   return (
-    <div className="flex justify-center items-center">
+    <div className= " w-full flex justify-center items-center">
       <div className="  space-y-4 flex flex-col items-center max-w-lg text-center text-stone-600 text-md">
         <Image
           alt="Coming soon illustration"
@@ -20,16 +20,16 @@ const ComingSoon = () => {
         <h2 className="text-stone-800 text-3xl font-SemiBold font-serif dark:text-gray-50">
           Under construction!
         </h2>
-        <div className={`text-stone-600 text-md space-y-4 font-medium dark:text-gray-200`}>
-          <p>🚀 Launching Soon – Experience the Future of AI!</p>
+        <div className={`text-stone-600 text-md space-y-4 font-light dark:text-gray-200`}>
+          <p className=" text-gray-150 font-semibold">🚀 Now in Beta – Empowering the Future of AI</p>
+         
           <p>
-          We are excited to announce the upcoming launch of our Beta Version!
-          </p>
-          <p>
-          Join us as we transform the AI landscape with advanced GPU renting and AI-driven solutions. 
-          Our platform empowers enterprises by providing unparalleled computational resources, and readily available AI models,
+          Our platform empowers enterprises by computational resources and readily available AI models,
           enabling innovation and accelerating growth.
-          Stay connected for updates and become part of the AI technology.
+          <br />
+          <span className="text-stone-300 font-semibold">
+            Stay tuned – groundbreaking features are on the horizon for our official release.
+            </span>
           </p>
           <p>
             If there is anything you would like to share with us, feel free to reach out!
