149f278c01fab73ede5241417cd9b3ad95dc61e5
nitishmeswal <nitish.meswal@gmail.com>
2025-01-01 19:52:06 +0530

    M: app/(main-components)/ai-models/page.tsx,

--- app/(main-components)/ai-models/page.tsx
+++ app/(main-components)/ai-models/page.tsx
@@ -17,15 +17,6 @@ const Home = () => {
   }
   return (
     <div className={`flex flex-1 w-full p-4 flex-col overflow-hidden`}>
-      <div className="flex flex-row p-4 justify-start">
-        {AIModelData.map((filter, index) => (
-          <FilterMenu
-            key={index} 
-            name={filter.name} 
-            filters={filter.options}
-          />
-        ))}
-      </div>
       <div className="flex justify-end flex-col py-4 transition-all duration-300">
         <AiModels/>
       </div>
